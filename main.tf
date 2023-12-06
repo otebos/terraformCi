6188eb8edc65a4e368233bab89ed8a349fa511ec
otebos <otebos@gmail.com>
2023-12-06 20:55:49 +0100

    upd 3

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "strgbsts${random_string.uniquestring.result}"
+  name                     = "leping${random_string.uniquestring.result}"
   location                 = azurerm_resource_group.rg.location
   resource_group_name      = azurerm_resource_group.rg.name
   account_tier             = "Standard"
